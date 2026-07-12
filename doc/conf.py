a9ee6475c0356a959857b633508cb4863ea25da4
parulin <161326115+parulin@users.noreply.github.com>
2026-07-12 15:48:08 +0000

    Mock rpm module when building documentation

--- doc/conf.py
+++ doc/conf.py
@@ -131,6 +131,9 @@ pygments_style = 'sphinx'
 # If true, `todo` and `todoList` produce output, else they produce nothing.
 todo_include_todos = True
 
+autodoc_mock_imports = [
+    'rpm',
+]
 
 # -- Options for HTML output ----------------------------------------------
 
